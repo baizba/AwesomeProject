27d9433032670c1bbed27613a7c993e67fb6bd9c
Vidovic Branislav <branislav.vidovic@ext.a1.at>
2024-05-01 18:59:57 +0200

    maps finally working

--- Map.tsx
+++ Map.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import { LatLng, LeafletView } from 'react-native-leaflet-view';
+
+const DEFAULT_COORDINATE: LatLng = {
+  lat: 44.606388,
+  lng: 17.859719,
+};
 
 const Map = () => {
   return (
-    <View>
-      <Text>Maps will be here</Text>
-    </View>
-  );
+      <View style={styles.root}>
+        <LeafletView
+          mapMarkers={[
+            {
+              position: DEFAULT_COORDINATE,
+              icon: '📍',
+              size: [32, 32],
+            },
+          ]}
+          mapCenterPosition={DEFAULT_COORDINATE}
+        />
+      </View>
+    );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Map;
